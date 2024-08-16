4e68f1483f9ddb43e2463265be0079a34ed7400e
sameedmajgaonkar <sameed1718@gmail.com>
2024-08-17 01:47:27 +0530

    Building game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import apiClient, { CanceledError } from "../services/apiClient";
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 interface FetchGamesResponse {
   count: number;
