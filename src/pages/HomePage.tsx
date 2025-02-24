6ea5f90aecf71c4e823239892264f922018fbb16
Sameed Majgaonkar <sameed1718@gmail.com>
2025-02-24 21:28:21 +0530

    Final Touchup

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,6 +6,7 @@ import PlatformSelector from "../components/PlatformSelector";
 import SortSelector from "../components/SortSelector";
 
 const HomePage = () => {
+  console.log(import.meta.env.VITE_KEY);
   return (
     <Grid
       templateAreas={{
--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -9,7 +9,7 @@ interface FetchResponse<T> {
 const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "85ebb5fe85694e8daeae5ee3f701d086",
+    key: import.meta.env.VITE_RAWG_API_KEY,
   },
 });
 
